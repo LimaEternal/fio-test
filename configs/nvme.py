501d437d5e1ba39a6291c22294034fb5d5c71913
Lima <limaeternal@gmail.com>
2026-08-03 18:54:50 +0300

    правка конфига для nvme

--- configs/nvme.py
+++ configs/nvme.py
@@ -10,26 +10,26 @@ TESTS = [
     {
         "id": "seq_read",
         "name": "1. Послед. Чтение",
-        "args": ["--rw=read", "--bs=128k", "--iodepth=128", "--numjobs=1",
-                 "--runtime=30", "--time_based", "--direct=1", "--ioengine=libaio"],
+        "args": ["--rw=read", "--bs=128k", "--iodepth=32", "--numjobs=1",
+                 "--runtime=30", "--time_based", "--ioengine=libaio", "--direct=1"],
     },
     {
         "id": "seq_write",
         "name": "2. Послед. Запись",
-        "args": ["--rw=write", "--bs=128k", "--iodepth=128", "--numjobs=1",
-                 "--runtime=30", "--time_based", "--direct=1", "--ioengine=libaio"],
+        "args": ["--rw=write", "--bs=128k", "--iodepth=32", "--numjobs=1",
+                 "--runtime=30", "--time_based", "--fsync=1"],
     },
     {
         "id": "rand_read",
         "name": "3. Случ. Чтение 4k",
         "args": ["--rw=randread", "--bs=4k", "--iodepth=32", "--numjobs=8",
-                 "--runtime=30", "--time_based", "--direct=1", "--ioengine=libaio"],
+                 "--runtime=30", "--time_based", "--fsync=1"],
     },
     {
         "id": "rand_write",
         "name": "4. Случ. Запись 4k",
         "args": ["--rw=randwrite", "--bs=4k", "--iodepth=32", "--numjobs=8",
-                 "--runtime=30", "--time_based", "--direct=1", "--ioengine=libaio"],
+                 "--runtime=30", "--time_based", "--fsync=1"],
     },
 ]
 
